c67e9276a29a8d49fb585bcc4bdcbc42a531c9d3
HadaluZurita <80125944+HadaluZurita@users.noreply.github.com>
2021-04-15 23:39:31 -0500

    Practica 1

---
+++ Practica1Unidad1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Practica1Unidad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Alumno = 0;
+            Console.WriteLine("Ingresar la cantidad de alumnos");
+            Alumno = Convert.ToInt32(Console.ReadLine());
+            decimal[,] arreglocalif = new decimal[Alumno, 2];
+            Console.WriteLine();
+
+            for (int i = 0; i <= Alumno-1; i++)
+            {
+                Console.WriteLine("Calificacion del alumno N."+(i+1).ToString());
+                arreglocalif[i, 0]=Convert.ToDecimal(Console.ReadLine());
+                arreglocalif[i, 1] = (i + 1);//Alumno
+                Console.WriteLine();
+            }
+            decimal calif = 0;
+            decimal Alumno2 = 0;
+            for (int i = 0; i <= Alumno - 1; i++)
+            {
+                if (arreglocalif[i,0]>calif)
+                {
+                    calif = arreglocalif[i, 0];
+                    Alumno2 = arreglocalif[i, 1] = (i + 1);
+                }
+            }
+            Console.WriteLine("El alumno con el mejor promedio es el alumno n. {0} con una calificacion de {1}",
+            Alumno2.ToString(),
+            calif.ToString());
+            Console.WriteLine();
+
+            calif = 10;
+            Alumno2 = 0;
+            for (int i = 0; i <= Alumno-1; i++)
+            {
+                if (arreglocalif[i,0] < calif)
+                {
+                    calif = arreglocalif[i, 0];
+                    Alumno2 = arreglocalif[i, 1] = (i + 1);
+                }
+            }
+            Console.WriteLine("El alumno con el peor promedio es el alumno n. {0} con una calificacion de {1}",
+            Alumno2.ToString(),
+            calif.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
